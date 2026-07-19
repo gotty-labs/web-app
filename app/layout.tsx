a206ebc78a68e05134f81b017175e90de6852745
0xEsp <espeso86@gmail.com>
2026-07-19 18:11:25 +0200

    feat(app): add Vercel Speed Insights to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Geist_Mono, Montserrat, Source_Sans_3 } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 import { env } from '@/lib/config/env'
 
@@ -44,6 +45,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
